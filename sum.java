84f45f00d8f4df2474ffb3b396df1f12537bca78
AditiUtreja01 <80740237+AditiUtreja01@users.noreply.github.com>
2021-03-16 23:01:29 +0530

    Create sum.java

---
+++ sum.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.util.Scanner;
+public class Main {
+    public static void main(String args[]) {
+        int sum,num1,num2;
+        Scanner inp=new Scanner(System.in);
+        num1=inp.nextInt();
+        num2=inp.nextInt();
+        sum=num1+num2;
+        System.out.println(sum);
+    }
+}
